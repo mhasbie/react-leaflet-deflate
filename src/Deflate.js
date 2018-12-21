a8f44ccfdcfb66b1970baf80148648f2d8fedbb1
Clint Harris <clintharris@users.noreply.github.com>
2018-12-21 17:45:03 -0500

    Fixes regressions that broke the library; specifcally changes that were made to src/Deflate.src on 2018/9/26. package.json and package-lock.json were not deliberately modified; running npm install generated modified package-lock.json.

--- src/Deflate.js
+++ src/Deflate.js
@@ -1,29 +1,27 @@
 import Leaflet from 'leaflet';
 import { DeflatedFeatureGroup } from 'Leaflet.Deflate';
-import { MapLayer } from 'react-leaflet';
-import 'leaflet.markercluster';
+import { MapLayer, withLeaflet } from 'react-leaflet';
 
 
-export default class Deflate extends MapLayer {
+class Deflate extends MapLayer {
 
 	createLeafletElement(props) {
 		return new DeflatedFeatureGroup(props);
 	}
 
-	componentDidMount() {
-		const { map } = this.props.leaflet || this.context;
-		const { markerCluster } = this.props;
+  componentDidMount() {
 		// This will add our DeflatedFeatureGroup (this.leafletElement) to the map--something that must happen BEFORE
 		// we can add anything to DeflatedFeatureGroup itself.
-		super.componentDidMount();
+    super.componentDidMount();
 		Leaflet.geoJSON(this.props.data, this.props).addTo(this.leafletElement);
-		if (markerCluster) map._container.className += ' leaflet-cluster-anim marker-cluster-styled marker-cluster-animated';
-	}
+  }
 
-	updateLeafletElement(fromProps, toProps) {
+  updateLeafletElement(fromProps, toProps) {
 		this.props.leaflet.layerContainer.removeLayer(this.leafletElement);
 		this.leafletElement = new DeflatedFeatureGroup(toProps);
 		this.props.leaflet.layerContainer.addLayer(this.leafletElement);
 		Leaflet.geoJSON(toProps.data, toProps).addTo(this.leafletElement);
 	}
 }
+
+export default withLeaflet(Deflate);
